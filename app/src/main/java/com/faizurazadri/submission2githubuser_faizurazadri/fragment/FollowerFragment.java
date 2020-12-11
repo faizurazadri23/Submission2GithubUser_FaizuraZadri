7a999e25fd671b1f05c3bf54e3c8596fb1193c17
faizurazadri23 <faizurazadri08@gmail.com>
2020-12-11 22:20:12 +0700

    Optimasi Kode

--- app/src/main/java/com/faizurazadri/submission2githubuser_faizurazadri/fragment/FollowerFragment.java
+++ app/src/main/java/com/faizurazadri/submission2githubuser_faizurazadri/fragment/FollowerFragment.java
@@ -30,19 +30,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FollowerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FollowerFragment extends Fragment {
 
     RecyclerView recyclerViewFollower;
     UserModel datauser;
 
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -51,18 +45,10 @@ public class FollowerFragment extends Fragment {
     private String mParam2;
 
     public FollowerFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FollowerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FollowerFragment newInstance(String param1, String param2) {
         FollowerFragment fragment = new FollowerFragment();
         Bundle args = new Bundle();
@@ -84,7 +70,7 @@ public class FollowerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_follower, container, false);
     }
 
--- app/src/main/java/com/faizurazadri/submission2githubuser_faizurazadri/fragment/FollowingFragment.java
+++ app/src/main/java/com/faizurazadri/submission2githubuser_faizurazadri/fragment/FollowingFragment.java
@@ -31,18 +31,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FollowingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FollowingFragment extends Fragment {
     
     RecyclerView recyclerViewFollowing;
     UserModel userModel;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -51,18 +45,9 @@ public class FollowingFragment extends Fragment {
     private String mParam2;
 
     public FollowingFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FollowingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FollowingFragment newInstance(String param1, String param2) {
         FollowingFragment fragment = new FollowingFragment();
         Bundle args = new Bundle();
@@ -84,7 +69,7 @@ public class FollowingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_following, container, false);
     }
 
